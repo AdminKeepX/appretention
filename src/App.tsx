2db763707e25a2c015d076e1e1058d6206b65967
PavelVaskou <3635019@gmail.com>
2024-05-22 22:20:27 +0300

    Appbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
+import AppAppBar from "./components/MainAppBar";
 
 function App() {
   const { network } = useTonConnect();
 
   return (
-    <TonConnectButton />  
+    <AppAppBar/>
+    // <TonConnectButton />  
   );
 }
 
