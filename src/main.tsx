dd00012574ad57196106e6c885d6eac176636dfa
PavelVaskou <3635019@gmail.com>
2024-06-06 21:35:08 +0300

    viewpoer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+
 import { createTheme, ThemeProvider } from "@mui/material";
 // this manifest is used temporarily for development purposes
 const manifestUrl =
