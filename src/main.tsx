8dd91fc77ba09abf2721cdf57a308d7b62267202
PavelVaskou <3635019@gmail.com>
2024-06-11 14:19:24 +0300

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 // this manifest is used temporarily for development purposes
 const manifestUrl =
   "https://raw.githubusercontent.com/AdminKeepX/description/main/manifest.json";
@@ -14,12 +14,31 @@ const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
-const theme = createTheme()
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#0000ff',
+    },
+    secondary: {
+      // main: '#dc004e',
+      main: '#FF0000',
+    },
+    background: {
+      default: '#f0FF0f',
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif',
+  },
+  // Добавьте другие настройки темы по необходимости
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
         <App />
       </ThemeProvider>
     </QueryClientProvider>
